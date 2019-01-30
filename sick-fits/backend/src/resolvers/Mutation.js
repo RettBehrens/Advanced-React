ad07f37c23e29d823aef88f15006d773f208fa01
RettBehrens <everettbehrens@gmail.com>
2019-01-29 18:34:43 -0700

    committing progress

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, arguments, context, info) {
+    const item = await context.db.mutation.createItem(
+      {
+        data: {
+          ...arguments
+        }
+      },
+      info
+    );
+    return item;
+  }
+};
 
-module.exports = mutations;
+module.exports = Mutations;
